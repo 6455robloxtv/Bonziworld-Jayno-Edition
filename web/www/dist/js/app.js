eac3f186cc8bfe0da89f5139a72865456ba9fdba
e_rafealplsleavemealone <6455robloxtv@gmail.com>
2025-03-04 18:08:04 +0000

    Update app.js

--- web/www/dist/js/app.js
+++ web/www/dist/js/app.js
@@ -902,7 +902,7 @@ _this2.userPublic.color_cross.match(/gffgfghjghj/g)) {
 								} else if (this.userPublic.voice.match(/voiceforge\:/i)) {
 									
 									var voice2;
-                                    this.userPublic.a = new Audio("https://mespeak-engine.onrender.com/voiceforge?text=" + encodeURIComponent(say) + "&voice="+ encodeURIComponent(replaceAll(this.userPublic.voice,"voiceforge:","")));
+                                    this.userPublic.a = new Audio("https://mespeak-js-server-2.onrender.com/voiceforge?text=" + encodeURIComponent(say) + "&voice="+ encodeURIComponent(replaceAll(this.userPublic.voice,"voiceforge:","")));
                                     this.userPublic.a.play();
                                     this.userPublic.a.onended = function() {
                                         self.clearDialog()
